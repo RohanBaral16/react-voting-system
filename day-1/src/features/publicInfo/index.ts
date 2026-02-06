81c37fc5560b66cdad4bdcb37d08b0611bb56ab6
rohanbaral16 <rhnbrl16@gmail.com>
2026-02-06 09:49:22 +0545

    some changes

--- day-1/src/features/publicInfo/index.ts
+++ day-1/src/features/publicInfo/index.ts
@@ -2,4 +2,5 @@ export { default as ElectionInfo } from "./ElectionInfo";
 export { default as DemoBooth } from "./DemoBooth";
 export { default as CandidatesInfo } from "./CandidatesInfo";
 export { default as ElectionResults } from "./ElectionResults";
-export { default as Home } from "./Home";export { default as NotFound } from "./NotFound";
+export { default as Home } from "./Home";
+export { default as NotFound } from "./NotFound";
